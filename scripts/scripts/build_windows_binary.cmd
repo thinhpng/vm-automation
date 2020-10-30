1b981599913be3a49a8ad82600bed1b7255b0f18
Pernat1y <1ns@pm.me>
2020-10-30 11:07:26 +0200

    Delete build_windows_binary.cmd

--- scripts/scripts/build_windows_binary.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-@echo Building Windows binary...
-cd ..
-call "C:\Program Files\Python38\Scripts\nuitka.bat" --standalone --remove-output demo_cli_no_version_check.py
-move demo_cli_no_version_check.dist\demo_cli_no_version_check.exe vm_automation_windows.exe
-upx.exe -q -9 vm_automation_windows.exe
